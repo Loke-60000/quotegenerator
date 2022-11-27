a36895e07880c8d880995b3dbc62818dd14c6b22
Lokman <104599813+Loke-60000@users.noreply.github.com>
2022-11-27 20:23:21 +0100

    Delete communism.js

--- js/communism.js
+++
@@ -1,8 +0,0 @@
-const audio = new Audio("img/S3K_63.WAV");
-const buttons = document.querySelectorAll("coolbutton");
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
